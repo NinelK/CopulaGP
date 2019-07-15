792df3e3f162a61fe8b74f69549da54235ea661d
NinelK <nkudryas@ed.ac.uk>
2019-07-15 16:31:51 +0100

    Add KISS_GP model

--- bvcopula/models.py
+++ bvcopula/models.py
@@ -33,3 +33,35 @@ class GPInferenceModel(gpytorch.models.PyroVariationalGP):
         mean = self.mean_module(x)  # Returns an n_data vec
         covar = self.covar_module(x)
         return gpytorch.distributions.MultivariateNormal(mean, covar)
+
+class KISS_GPInferenceModel(gpytorch.models.PyroVariationalGP):
+    def __init__(self, likelihood, grid_size=128, grid_bounds=[(0, 1)], name_prefix="mixture_gp"):
+        # Define all the variational stuff
+        variational_distribution = gpytorch.variational.CholeskyVariationalDistribution(grid_size)
+        variational_strategy = gpytorch.variational.GridInterpolationVariationalStrategy(
+            self, grid_size, grid_bounds, variational_distribution
+        )
+        
+        # Standard initializtation
+        super(GPInferenceModel2, self).__init__(variational_strategy, likelihood, num_data=grid_size) #?
+        self.likelihood = likelihood
+        
+        # Mean, covar
+        self.mean_module = gpytorch.means.ConstantMean()
+        
+        #we specify prior here
+        prior_rbf_length = 0.5 
+        lengthscale_prior = gpytorch.priors.NormalPrior(prior_rbf_length, 1.0) #variance does not matter much
+        
+        self.covar_module = gpytorch.kernels.ScaleKernel(
+            gpytorch.kernels.RBFKernel(lengthscale_prior=lengthscale_prior),
+        )
+        
+        # Initialize lengthscale and outputscale to mean of priors
+        self.covar_module.base_kernel.lengthscale = lengthscale_prior.mean
+        #self.covar_module.outputscale = outputscale_prior.mean
+
+    def forward(self, x):
+        mean = self.mean_module(x)  # Returns an n_data vec
+        covar = self.covar_module(x)
+        return gpytorch.distributions.MultivariateNormal(mean, covar)
