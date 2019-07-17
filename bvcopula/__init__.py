dcd2647d1feac3f0aca5a8953962060770283839
NinelK <nkudryas@ed.ac.uk>
2019-07-17 17:10:06 +0100

    Add GaussCopula transformed from MultivariateNormal

--- bvcopula/__init__.py
+++ bvcopula/__init__.py
@@ -1,3 +1,4 @@
 from .distributions import GaussianCopula
-from .likelihoods import GaussianCopula_Likelihood
+from .likelihoods import GaussianCopula_Likelihood, GaussianCopula_Flow_Likelihood
 from .models import GPInferenceModel, KISS_GPInferenceModel
+from .dist_transform import NormTransform
--- bvcopula/likelihoods.py
+++ bvcopula/likelihoods.py
@@ -4,8 +4,10 @@ from typing import Any
 from gpytorch.likelihoods.likelihood import Likelihood
 from gpytorch.distributions import MultivariateNormal, base_distributions
 from gpytorch.utils.deprecation import _deprecate_kwarg_with_transform
+from torch.distributions.transformed_distribution import TransformedDistribution #for Flow
 
 from .distributions import GaussianCopula
+from .dist_transform import NormTransform
 
 class GaussianCopula_Likelihood(Likelihood):
     def __init__(self, noise_prior=None, noise_constraint=None, batch_shape=torch.Size(), **kwargs: Any):
@@ -31,3 +33,42 @@ class GaussianCopula_Likelihood(Likelihood):
     def forward(self, function_samples: Tensor, *params: Any, **kwargs: Any) -> GaussianCopula:
         scale = self.gplink_function(function_samples)
         return GaussianCopula(scale)
+
+
+class GaussianCopula_Flow_Likelihood(Likelihood):
+    def __init__(self, noise_prior=None, noise_constraint=None, batch_shape=torch.Size(), **kwargs: Any):
+        batch_shape = _deprecate_kwarg_with_transform(
+            kwargs, "batch_size", "batch_shape", batch_shape, lambda n: torch.Size([n])
+        )
+        super(Likelihood, self).__init__()
+        self._max_plate_nesting = 1
+    
+    @staticmethod
+    def gplink_function(f: Tensor) -> Tensor:
+        """
+        GP link function transforms the GP latent variable `f` into :math:`\theta`,
+        which parameterizes the distribution in :attr:`forward` method as well as the
+        log likelihood of this distribution defined in :attr:`expected_log_prob`.
+        """
+        if f.is_cuda:
+            get_cuda_device = f.get_device()
+            return (2*base_distributions.Normal(torch.zeros(1).cuda(device=get_cuda_device),torch.ones(1).cuda(device=get_cuda_device)).cdf(f) - 1)
+        else:
+            return (2*base_distributions.Normal(0,1).cdf(f) - 1)
+    
+    @staticmethod
+    def corr_mat(X: Tensor) -> Tensor:
+        """
+        Constructs a batch of correlation matrices with '\rho' = Tensor.
+        Batch size = batch_size of `\rho`
+        """
+        corr_mat = torch.stack([torch.stack([torch.ones_like(X),X]),
+                                torch.stack([X,torch.ones_like(X)])])
+        corr_mat = torch.einsum('ij...->...ij', corr_mat)
+        return corr_mat
+    
+    def forward(self, function_samples: Tensor, *params: Any, **kwargs: Any) -> GaussianCopula:
+        scale = self.gplink_function(function_samples)
+        base_dist = base_distributions.MultivariateNormal(torch.zeros(scale.shape + torch.Size([2])),
+                                                          self.corr_mat(scale))
+        return TransformedDistribution(base_dist, NormTransform())
